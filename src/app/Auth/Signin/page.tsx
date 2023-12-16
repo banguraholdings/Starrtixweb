2610b5565ba4b632a4e99f6df75f8004c65b55a5
Judah40 <judahdore@gmail.com>
2023-12-16 02:56:04 +0000

    pics

--- src/app/Auth/Signin/page.tsx
+++ src/app/Auth/Signin/page.tsx
@@ -6,7 +6,7 @@ function Signin() {
       <div className=" h-[100vh]  hidden w-full lg:block">
         <div className="lg:grid lg:grid-cols-2 lg:place-items-center h-full p-20 bg-blue-500">
           <div className="w-full h-full p-6 bg-blue-300 border-t-8 border-l-8">
-            <img src="/background/main2.jpg" alt="" className="border-4" />
+            <img src="background/main2.jpg" alt="" className="border-4" />
           </div>
           <div className="w-full h-full p-6 bg-blue-300 flex items-center justify-center font-serif text-lg">
           Seats So Good, You&apos;ll Feel Like Part of the Show!
@@ -16,10 +16,10 @@ function Signin() {
           </div>
           <div className="w-full h-full p-6 bg-blue-300">
             {" "}
-            <img src="/images/culture.jpg" alt="" className="border-4" />
+            <img src="images/culture.jpg" alt="" className="border-4" />
           </div>
           <div className="w-full h-full p-6 bg-blue-300">
-            <img src="/images/event2.jpg" alt="" className="border-4" />
+            <img src="images/event2.jpg" alt="" className="border-4" />
           </div>
           <div className="w-full h-full p-6 bg-blue-300 border-b-8 border-r-8 border-black flex items-center justify-center font-serif text-lg">
           More than Just a Ticket – An Experience!
